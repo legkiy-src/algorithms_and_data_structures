f56bb8d7929024584c2e6bfe462cf6a4c5bab013
Legkiy Andrey <legkiy.info@mail.ru>
2024-07-18 20:09:31 +0300

    Бинарный поиск в массиве

--- search/binary_search.php
+++ search/binary_search.php
@@ -1,1 +1,34 @@
 <?php
+
+function find(array $sampleList, int $searchKey): ?int
+{
+    $countItems = count($sampleList);
+    $lowerIndex = 0;
+    $upperIndex = $countItems - 1;
+
+    while (true) {
+        $currentIndex = round(($lowerIndex + $upperIndex) / 2);
+
+        if (!array_key_exists($currentIndex, $sampleList)) {
+            return null;
+        }
+
+        if ($sampleList[$currentIndex] === $searchKey) {
+            return $currentIndex;
+        } elseif ($lowerIndex > $upperIndex) {
+            return null;
+        } else {
+            if ($sampleList[$currentIndex] < $searchKey) {
+                $lowerIndex = $currentIndex + 1;
+            } else {
+                $upperIndex = $currentIndex - 1;
+            }
+        }
+    }
+}
+
+$sampleList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var_dump(find($sampleList, 5));
+
+var_dump(find($sampleList, 15));
